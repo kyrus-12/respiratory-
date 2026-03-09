533e3a88aae2ea64f3a9723f564be3d5aa14f754
kyrus-12 <rusky0537@gmail.com>
2026-03-09 17:32:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -32,5 +32,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
-
+// THIS SECTION IS UPDATED FOR RENDER
+const PORT = process.env.PORT || 3000; 
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
